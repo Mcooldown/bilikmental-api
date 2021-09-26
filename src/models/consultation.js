9b1652517a2d4936fe0932843cc59a89da693a75
Mcooldown <vincenthadinata30@gmail.com>
2021-09-27 00:49:30 +0700

    create all model

---
+++ src/models/consultation.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const Consultation = new Schema({
+     user: {type: Schema.Types.ObjectId, required: true, ref: 'User'},
+     consultant: {type: Schema.Types.ObjectId, required: true, ref: 'Consultant'},
+     date: {type: Date, required: true },
+     shift: { type: String, required: true},
+},{
+     timestamps: true,
+});
+
+module.exports = mongoose.model('Consultation', Consultation);
