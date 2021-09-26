3e6c959686224e8b598b951c94254cbb6f7b7f7d
Mcooldown <vincenthadinata30@gmail.com>
2021-09-27 00:02:59 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const path = require('path');
+
+const app = express();
+
+app.use(cors());
+
+app.use(express.json({limit: "50mb"}));
+app.use(express.urlencoded({ extended:true, limit: "50mb"}));
+
+app.use((error, req, res, next) => {
+
+     console.log(error);
+     const status = error.errorStatus || 500;
+     const message = error.errorMessage;
+     const data = error.data;
+
+     res.status(status).json({message: message, data: data});
+});
+
+mongoose.connect("mongodb+srv://vincenthadinata:bztYTO01Lk96QA8a@cluster0.gfq8o.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+.then(() => {
+     app.listen(process.env.PORT || 4000, () => console.log("Connection success"));
+})
+.catch( err => console.log(err));
