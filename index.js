6b6c52ee5368fc36360305610d15e50ee0025ba7
Mcooldown <vincenthadinata30@gmail.com>
2021-10-18 12:20:18 +0700

    cors

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 
 const app = express();
 
-app.use(cors());
+app.use(cors({origin: true}));
 
 app.use(express.json({limit: "50mb"}));
 app.use(express.urlencoded({ extended:true, limit: "50mb"}));
