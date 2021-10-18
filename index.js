d08337f3ade6e9140b4a1c505ba947d9885b0e36
Mcooldown <vincenthadinata30@gmail.com>
2021-10-18 12:25:50 +0700

    asdasda

--- index.js
+++ index.js
@@ -13,15 +13,15 @@ const authRoutes = require('./src/routes/auth');
 const quoteRoutes = require('./src/routes/quote');
 const commentRoutes = require('./src/routes/comment');
 const articleRoutes = require('./src/routes/article');
-// const consultantRoutes = require('./src/routes/consultant');
-// const consultationRoutes = require('./src/routes/consultation');
+const consultantRoutes = require('./src/routes/consultant');
+const consultationRoutes = require('./src/routes/consultation');
 // const meditationRoutes = require('./src/routes/meditation');
 app.use('/v1/auth', authRoutes);
 app.use('/v1/quotes', quoteRoutes);
 app.use('/v1/comments', commentRoutes);
 app.use('/v1/articles', articleRoutes);
-// app.use('/v1/consultants', consultantRoutes);
-// app.use('/v1/consultations', consultationRoutes);
+app.use('/v1/consultants', consultantRoutes);
+app.use('/v1/consultations', consultationRoutes);
 // app.use('/v1/meditations', meditationRoutes);
 
 app.use((error, req, res, next) => {
