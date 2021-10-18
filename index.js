842690cd6381952ab4559d3510fbf7d0e1941148
Mcooldown <vincenthadinata30@gmail.com>
2021-10-18 12:23:42 +0700

    adssad

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 
 const app = express();
 
-app.use(cors({origin: true}));
+app.use(cors());
 
 app.use(express.json({limit: "50mb"}));
 app.use(express.urlencoded({ extended:true, limit: "50mb"}));
@@ -13,16 +13,16 @@ const authRoutes = require('./src/routes/auth');
 const quoteRoutes = require('./src/routes/quote');
 const commentRoutes = require('./src/routes/comment');
 const articleRoutes = require('./src/routes/article');
-const consultantRoutes = require('./src/routes/consultant');
-const consultationRoutes = require('./src/routes/consultation');
-const meditationRoutes = require('./src/routes/meditation');
+// const consultantRoutes = require('./src/routes/consultant');
+// const consultationRoutes = require('./src/routes/consultation');
+// const meditationRoutes = require('./src/routes/meditation');
 app.use('/v1/auth', authRoutes);
 app.use('/v1/quotes', quoteRoutes);
 app.use('/v1/comments', commentRoutes);
 app.use('/v1/articles', articleRoutes);
-app.use('/v1/consultants', consultantRoutes);
-app.use('/v1/consultations', consultationRoutes);
-app.use('/v1/meditations', meditationRoutes);
+// app.use('/v1/consultants', consultantRoutes);
+// app.use('/v1/consultations', consultationRoutes);
+// app.use('/v1/meditations', meditationRoutes);
 
 app.use((error, req, res, next) => {
 
